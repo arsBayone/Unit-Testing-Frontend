4c330baeb0459295cfc40452858c5ac133c452b2
arsBayone <sanjum@bayone.com>
2025-04-23 15:10:45 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React Counter Demo</h2>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
